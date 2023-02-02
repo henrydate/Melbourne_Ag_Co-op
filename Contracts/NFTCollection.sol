02285f35fa83e53d36764b3678b508c65d0ced48
Akhil Sai Kavuri <109653396+kavuri-akhil@users.noreply.github.com>
2023-02-02 20:29:30 +1100

    Amended import statements

--- Contracts/NFTCollection.sol
+++ Contracts/NFTCollection.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.8.4;
 // Importing required libraries
- import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/591c12d22de283a297e2b551175ccc914c938391/contracts/token/ERC721/ERC721.sol";
+ /*import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/591c12d22de283a297e2b551175ccc914c938391/contracts/token/ERC721/ERC721.sol";
  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/591c12d22de283a297e2b551175ccc914c938391/contracts/access/Ownable.sol";
  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/591c12d22de283a297e2b551175ccc914c938391/contracts/utils/Counters.sol";
  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/591c12d22de283a297e2b551175ccc914c938391/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+*/
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
  // This is a simple NFT contract
  //Start of a contract
@@ -13,7 +19,7 @@ pragma solidity ^0.8.4;
     using Counters for Counters.counter;
 
     // Specifing the maximum supply
-    uint256 maxSupply = 10000;
+    uint256 maxSupply = 1000000;
     // Initiatina a counter to store to be able to use Counters on uint265 without impacting the rest of the uint256 in the contract.
     //Counter helps to find every function from a single library to a type to reduce gass fee.
     Counters.Counter private _tokenIDCounter;
