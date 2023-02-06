c38c5a33ded426a176a32702f4f72d56b5c5f2bd
Akhil Sai Kavuri <109653396+kavuri-akhil@users.noreply.github.com>
2023-02-06 16:11:30 +1100

    Added mint function to NFTCollection

--- Contracts/NFTCollection.sol
+++ Contracts/NFTCollection.sol
@@ -12,9 +12,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
     address public staking;
     //Constructor holds the value of staking address. 
     // The name of the token is set to "Fruit" which holds the symbol "FRUIT"
+    //Start of Constructor
     constructor(address _staking) ERC721("Fruit", "FRUIT") {
         staking = _staking;
-    }
+    }//End of Constructor.
+    // The function mint allows us to add FRUIT Tokens into the supply which can only be done by owner.
+    //Start of function
+    function mint(address to, uint256 amount) external onlyowner{
+        //Using require clause to check the address is owners address.
+        require(msg.sender == staking,"Fruit::mint: only staking contract can mint");
+        _mint(to, amount);
+    }//End of function. 
+    
     
 
  }
