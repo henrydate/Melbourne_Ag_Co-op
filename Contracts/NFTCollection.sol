fd069ed37a6c3d0f6277abd041ee268bcbbef61d
Akhil Sai Kavuri <109653396+kavuri-akhil@users.noreply.github.com>
2023-02-06 17:07:08 +1100

    Added SPDX Lisence to all contracts

--- Contracts/NFTCollection.sol
+++ Contracts/NFTCollection.sol
@@ -1,7 +1,8 @@
+//SPDX-License-Identifier: UNLISENCED
 pragma solidity ^0.8.9;
 //Importing required Libraries
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
  // This is a simple NFT contract
@@ -11,19 +12,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
      // Address of staking contract.
     address public staking;
     //Constructor holds the value of staking address. 
-    // The name of the token is set to "Fruit" which holds the symbol "FRUIT"
+    // The name of the token is set to "Nut" which holds the symbol "NUT"
     //Start of Constructor
-    constructor(address _staking) ERC721("Fruit", "FRUIT") {
+    constructor(address _staking) ERC721("Nut", "NUT") {
         staking = _staking;
     }//End of Constructor.
-    // The function mint allows us to add FRUIT Tokens into the supply which can only be done by owner.
+    // The function mint allows us to add NUT Tokens into the supply which can only be done by owner.
     //Start of function
-    function mint(address to, uint256 amount) external onlyowner{
+    function mint(address to, uint256 amount) external {
         //Using require clause to check the address is owners address.
-        require(msg.sender == staking,"Fruit::mint: only staking contract can mint");
+        require(msg.sender == staking,"Nut::mint: only staking contract can mint");
         _mint(to, amount);
     }//End of function. 
-    //This function is used to take FRUIT Tokens out of circulation. 
+    //This function is used to take NUT Tokens out of circulation. 
     //Start of function
     function burn(uint256 amount)external onlyOwner {
         _burn(amount);
--- Contracts/RewardToken.sol
+++ Contracts/RewardToken.sol
@@ -1,3 +1,5 @@
+//SPDX-License-Identifier: UNLISENCED
+
 pragma solidity ^0.8.4;
 //Importing required contracts
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
