4945013b45997f2b7f379f52708ee0239b184ce2
Akhil Sai Kavuri <109653396+kavuri-akhil@users.noreply.github.com>
2023-02-06 15:57:42 +1100

    Added constructor to NFTCollection.sol

--- Contracts/NFTCollection.sol
+++ Contracts/NFTCollection.sol
@@ -6,8 +6,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
  // This is a simple NFT contract
  //Start of a contract
- //INitialising contract as a ERC721, ERC721Enumerable, Ownable contract.
+ //INitialising contract as a ERC721, Ownable contract.
  contract NFTCollection is Ownable, ERC721 {
+     // Address of staking contract.
+    address public staking;
+    //Constructor holds the value of staking address. 
+    // The name of the token is set to "Fruit" which holds the symbol "FRUIT"
+    constructor(address _staking) ERC721("Fruit", "FRUIT") {
+        staking = _staking;
+    }
+    
 
  }
     
